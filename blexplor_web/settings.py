96eb259f75112858967d4c5d6b43ec55bcae3b0e
marzig76 <marzig76@gmail.com>
2016-04-13 15:24:03 -0400

    add django settings for new address app

--- blexplor_web/settings.py
+++ blexplor_web/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'block.apps.BlockConfig',
+    'address.apps.AddressConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
