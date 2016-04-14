3835721251ce5a69df71c8303b2392092542b52f
marzig76 <marzig76@gmail.com>
2016-04-14 16:59:47 -0400

    add tx inputs and outputs to template

--- block/views.py
+++ block/views.py
@@ -9,11 +9,15 @@ def index(request):
 def height(request, height):
     b = Block.objects.get(block_height=height)
     txs = Tx.objects.filter(block_id=b.id)
+    inputs = TxInput.objects.filter(tx_id__in=txs)
+    outputs = TxOutput.objects.filter(tx_id__in=txs)
 
     template = loader.get_template('block/index.html')
     context = {
         'b': b,
         'txs': txs,
+        'inputs': inputs,
+        'outputs': outputs,
     }
     return HttpResponse(template.render(context, request))
 
