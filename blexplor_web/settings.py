1f352c70dec99f9afdc006ba4d64c43e88ed1514
Mike M <marzig76@gmail.com>
2016-04-17 19:31:32 -0400

    template for homepage

--- blexplor_web/settings.py
+++ blexplor_web/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'blexplor_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['blexplor_web/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
