270612ded18b6aa9b72d13d48da35632c5438401
Mike Marzigliano <marzig76@gmail.com>
2016-05-07 18:15:06 -0400

    exception handling

--- block/views.py
+++ block/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.exceptions import ObjectDoesNotExist
 from block.models import Block, Tx, TxInput, TxOutput
+
 
 def index(request):
     return HttpResponse("Block not specified")
 
+
 def height(request, height):
-    b = Block.objects.get(block_height=height)
-    txs = Tx.objects.filter(block_id=b.id)
-    inputs = TxInput.objects.filter(tx_id__in=txs)
-    outputs = TxOutput.objects.filter(tx_id__in=txs)
+    try:
+        b = Block.objects.get(block_height=height)
+        txs = Tx.objects.filter(block_id=b.id)
+        inputs = TxInput.objects.filter(tx_id__in=txs)
+        outputs = TxOutput.objects.filter(tx_id__in=txs)
 
-    template = loader.get_template('block/index.html')
-    context = {
-        'b': b,
-        'txs': txs,
-        'inputs': inputs,
-        'outputs': outputs,
-    }
-    return HttpResponse(template.render(context, request))
-
+        template = loader.get_template('block/index.html')
+        context = {
+            'b': b,
+            'txs': txs,
+            'inputs': inputs,
+            'outputs': outputs,
+            }
+        return HttpResponse(template.render(context, request))
+    except ObjectDoesNotExist:
+        return HttpResponse("Block does not exist")
