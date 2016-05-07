8c5ef0aedc89df2766ecbdc1fa07282398b5b424
Mike Marzigliano <marzig76@gmail.com>
2016-05-07 18:48:22 -0400

    made a function to convert satoshis to whole btc units

--- block/templatetags/block_extras.py
+++ block/templatetags/block_extras.py
@@ -8,3 +8,8 @@ register = template.Library()
 def date_format(value):
     return datetime.datetime.fromtimestamp(
             value).strftime('%Y-%m-%d %H:%M:%S')
+
+
+@register.filter
+def satoshis2btc(value):
+    return value / 100000000.0
