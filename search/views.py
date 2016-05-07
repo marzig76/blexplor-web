0549a4be618929a8caf4a4c830e0d64cb8f24c71
Mike Marzigliano <marzig76@gmail.com>
2016-05-07 17:30:58 -0400

    started the search logic

--- search/views.py
+++ search/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from block.models import Block
+from django.http import HttpResponseRedirect
 
 def index(request):
-    return HttpResponse("No search parameter specified")
+    search = request.GET['search']
+    if search != '':
+        if search.isnumeric():
+            return HttpResponseRedirect("/block/" + search)
+        else:
+            response = 'Address: ' + search
+    else:
+        response = 'No search parameter provided.'
+
+    return HttpResponse(response)
 
 def search_string(request, search_string):
     return HttpResponse("Search string: " + search_string)
