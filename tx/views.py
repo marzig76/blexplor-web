4b63b07451df399b150e3ae391d1334b6697faf4
marzig76 <marzig76@gmail.com>
2016-05-09 17:03:51 -0400

    created a transaction view

--- tx/views.py
+++ tx/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from block.models import Block, Tx, TxInput, TxOutput
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Transaction ID not specified")
+
+def tx(request, tx):
+    response = "Transaction: " + tx
+    return HttpResponse(response)
